c1db59399d43e28bdb9756d14666855865c91a59
Rayanki charan <rayankicharan32@gmail.com>
2025-11-20 17:22:05 +0530

    @primary annotation and the different ways of naming Bean

--- ThirdProject/src/main/java/com/appconfig/AppConfig.java
+++ ThirdProject/src/main/java/com/appconfig/AppConfig.java
@@ -2,6 +2,7 @@ package com.appconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.spring.model.Vehicle;
 @Configuration
@@ -18,6 +19,7 @@ public class AppConfig {
 		vehicle.setName("AUDI");
 		return vehicle;
 	}
+	@Primary
 	@Bean("Gwagon")
 	Vehicle vehicle4() {
 		Vehicle vehicle = new Vehicle();
--- ThirdProject/src/main/java/com/springlearning/ThirdProject/App.java
+++ ThirdProject/src/main/java/com/springlearning/ThirdProject/App.java
@@ -10,7 +10,12 @@ public class App
     public static void main( String[] args )
     {
        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+       // creting bean by name
        Vehicle veh = context.getBean("ferrari",Vehicle.class);
        System.out.println(veh.getName());
+       
+       // primary annotation without any name creates with primary as Default
+       Vehicle veh1 = context.getBean(Vehicle.class);
+       System.out.println(veh1.getName());
     }
 }
