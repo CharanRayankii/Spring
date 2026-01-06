7fc75038def66ca74aa98541da2a1f0d6297a39a
Rayanki charan <rayankicharan32@gmail.com>
2026-01-06 18:57:53 +0530

    xml way of creating beans

---
+++ example6/src/main/java/com/Rc/example6/App.java
@@ -0,0 +1,22 @@
+package com.Rc.example6;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.Rc.example6.beans.Vehicle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        Vehicle veh = (Vehicle)context.getBean("vehicle");
+        veh.setName("Audi - 8");
+        System.out.println(veh.getName());
+    }
+}
