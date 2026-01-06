30ea1a73b149505b86475a533d238060f93f8302
Rayanki charan <rayankicharan32@gmail.com>
2026-01-06 22:53:21 +0530

    learning how to create spring boot application

---
+++ SpringBoot1/src/main/java/com/Rc/SpringBoot1/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.Rc.SpringBoot1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String home() {
+		return "home.html";
+	}
+
+}
